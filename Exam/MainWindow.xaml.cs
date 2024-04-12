e8fe08c7193c76af37b44a4f03623b7705a52c77
Olia <oliaopanasiukk@gmail.com>
2024-04-12 13:52:12 +0300

    initial commit

---
+++ Exam/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Exam
+{
+
+    public partial class MainWindow : Window
+    {
+        public string Word { get; set; }
+        public string Source { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Button(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.InitialDirectory = Source;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                Source = dialog.FileName;
+                sourceTextBox.Text = Source;
+            }
+        }
+
+        private async void Search_Button(object sender, RoutedEventArgs e)
+        {
+            Word = wordTextBox.Text;
+            Source = sourceTextBox.Text;
+
+            if (string.IsNullOrEmpty(Word))
+            {
+                MessageBox.Show("Enter a word to search for");
+                return;
+            }
+            if (string.IsNullOrEmpty(Source))
+            {
+                MessageBox.Show("Specify the correct path");
+                return;
+            }
+            if (!File.Exists(Source) && !Directory.Exists(Source))
+            {
+                MessageBox.Show("The specified file or directory does not exist");
+                return;
+            }
+
+            await SearchWordAsync(Word, Source);
+        }
+        private async Task SearchWordAsync(string word, string source)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    string[] files = Directory.GetFiles(source, "*", SearchOption.AllDirectories);
+
+                    int allFiles = files.Length;
+                    int processed = 0;
+
+                    foreach (string file in files)
+                    {
+                        int occurrences = TheNumberOfWordOccurrences(file, word);
+
+                        processed++;
+
+                        int progress = (int)((double)processed / allFiles * 100);
+                        RefreshProgress(progress);
+                    }
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        var res = new List<ViewReport>();
+                        foreach (string file in files)
+                        {
+                            int occurrences = TheNumberOfWordOccurrences(file, word);
+                            res.Add(new ViewReport { Word = word, FileName = Path.GetFileName(file), Occurrences = occurrences });
+                        }
+                        ReportWin reportWin = new ReportWin(res);
+                        reportWin.Show();
+                        reportWin.Closed += (sender, e) => { percent.Value = 0; progressTextBox.Text = $"0%"; };
+                    });
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            });
+        }
+        private void RefreshProgress(int progress)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                percent.Value = progress;
+                progressTextBox.Text = $"{progress:0}%";
+            });
+        }
+
+        private int TheNumberOfWordOccurrences(string file, string word)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    Regex regex = new Regex(@"\b" + Regex.Escape(word) + @"\b", RegexOptions.IgnoreCase);
+                    MatchCollection matches = regex.Matches(line);
+                    count += matches.Count;
+
+                }
+            }
+            return count;
+
+        }
+    }
+
+}
---
+++ Exam/ReportWin.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Exam
+{
+    public partial class ReportWin : Window
+    {
+
+        public ReportWin(List<ViewReport> list)
+        {
+            InitializeComponent();
+            filesListView.ItemsSource = list;
+        }
+
+        private void SaveReport_Click(object sender, RoutedEventArgs e)
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string file = Path.Combine(desktop, "Report.txt");
+
+            if (File.Exists(file))
+            {
+                string newName = GenerateNewFileName(desktop, "Report.txt");
+                file = Path.Combine(desktop, newName);
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.WriteLine("Word\t\tFile Name\t\t\tOccurrences");
+
+                    foreach (var item in filesListView.Items)
+                    {
+                        var report = (ViewReport)item;
+                        writer.WriteLine($"{report.Word,-10}\t{report.FileName,-30}\t{report.Occurrences}");
+                    }
+                }
+                MessageBox.Show($"The report is saved on the desktop: {file}", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private string GenerateNewFileName(string dir, string initialFile)
+        {
+            int counter = 1;
+            string newFile = initialFile;
+
+            while (File.Exists(Path.Combine(dir, newFile)))
+            {
+                newFile = $"{Path.GetFileNameWithoutExtension(initialFile)}_{counter}{Path.GetExtension(initialFile)}";
+                counter++;
+            }
+            return newFile;
+        }
+    }
+
+    public class ViewReport
+    {
+        public string Word { get; set; }
+        public string FileName { get; set; }
+        public int Occurrences { get; set; }
+    }
+
+}
+
